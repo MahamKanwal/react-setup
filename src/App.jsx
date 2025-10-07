8eca1b1762b83b60d97daeeefee7c94794b4c35f
alimehdi110 <ykhizar2@gmail.com>
2025-10-07 15:46:02 +0500

    feat: initialize React project with Vite, Tailwind CSS, and ESLint
    - Added package.json with dependencies for React, Vite, and Tailwind CSS
    - Created postcss.config.js for Tailwind CSS and autoprefixer
    - Implemented main application structure in App.jsx
    - Added global styles using Tailwind CSS in index.css
    - Set up entry point in main.jsx to render the App component
    - Created Home component for the home page
    - Configured Tailwind CSS in tailwind.config.js
    - Set up Vite configuration in vite.config.js

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Home from "./pages/Home";
+
+const App = () => {
+const name = "ali"
+  return (
+    <>
+      <h1 className="text-5xl ">hello react</h1>
+      <h1>hello react</h1>
+      <h1>hello {name}</h1>
+      <Home />
+    </>
+  )
+}
+
+
+
+export default App;
